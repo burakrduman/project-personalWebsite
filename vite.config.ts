8ad7612afdeaaed13aa1313f1e0e70a0cf5f6289
Burak Duman <34383005+burakrduman@users.noreply.github.com>
2025-07-25 13:21:05 +0100

    Editted for GitHub pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-personalWebsite/', // Bu satırı kendi repository adınızla güncelleyin
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
